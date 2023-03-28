82b059cbb160bc8f29a76dd8956dc1af7cd5517d
edgar <edgarkhan13@yahoo.com>
2023-03-28 07:13:05 -0700

    update

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#define main.h
+#define MAIN.H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_to_go(int *n);
+void swap_int(void *a, int *b);
+int _strlen(char *s);
+int _puts(char *str);
+void _ptint_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int *n);
+char '_strcpy(char *dest, char *src);
